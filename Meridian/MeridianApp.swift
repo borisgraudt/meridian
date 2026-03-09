5f8f060272e65a1d91e573434a988e8caeb2e82e
borisgraudt <boris.graudt@gmail.com>
2026-03-09 08:39:43 +0300

    Initial Commit

---
+++ Meridian/MeridianApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeridianApp.swift
+//  Meridian
+//
+//  Created by boris on 3/9/26.
+//
+
+import SwiftUI
+
+@main
+struct MeridianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MeridianTests/MeridianTests.swift
@@ -0,0 +1,17 @@
+//
+//  MeridianTests.swift
+//  MeridianTests
+//
+//  Created by boris on 3/9/26.
+//
+
+import Testing
+@testable import Meridian
+
+struct MeridianTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
